ea39db68d4396176dbad1ecb3eef4a1d1a48dc20
niruhan <niruhanv@gmail.com>
2020-02-27 13:29:22 +0530

    Add build util for windows folder hiding

---
+++ features/org.wso2.carbon.runtime.feature/src/main/java/org/wso2/carbon/runtime/feature/BuiltUtils.java
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.wso2.carbon.runtime.feature;
+
+public class BuiltUtils {
+    public static void main(String[] args) {
+        System.out.println("##############Hello");
+//        Path path = ...;
+//        Boolean hidden = path.getAttribute("dos:hidden", LinkOption.NOFOLLOW_LINKS);
+//        if (hidden != null && !hidden) {
+//            path.setAttribute("dos:hidden", Boolean.TRUE, LinkOption.NOFOLLOW_LINKS);
+//        }
+    }
+}
